1d38aba20b288ac06d9cba7997145b4bd3872862
Darko <d.siracevski@gmail.com>
2024-12-27 16:25:51 +0100

    quickfix

--- resources/js/Components/Custom/Menu.vue
+++ resources/js/Components/Custom/Menu.vue
@@ -23,31 +23,6 @@ function changeMode() {
 
 <template>
   <div class="bg-transparent grid grid-cols-3 place-items-center gap-5 relative md:max-w-4xl lg:max-w-7xl lg:min-w-7xl min-w-[332px] mx-auto">
-    <button @click="darkMode = !darkMode" class="h-12 w-12 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-      <svg class="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-      </svg>
-      <svg class="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
-        <path
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-            fill-rule="evenodd" clip-rule="evenodd"></path>
-      </svg>
-    </button>
-    <div class="bg-transparent flex place-content-center gap-0 relative self-center left-1">
-      <Link
-          href="/"
-          class="relative top-0 left-3 border-t-2 border-l-2 border-white border-b-2 py-2 px-6 bg-[#C9E6F0] my-auto rounded-2xl rounded-r-none capitalize">
-        {{ $t('menu.home') }}
-      </Link>
-      <div class="relative z-10 border-2 text-white rounded-full">
-        <UpdateButton/>
-      </div>
-      <Link
-          href='/about'
-          class="relative top-0 -left-3 border-t-2 border-r-2 border-white border-b-2 py-2 px-6 bg-[#C9E6F0] my-auto rounded-2xl rounded-l-none capitalize">
-        {{ $t('menu.about') }}
-      </Link>
-    </div>
     <div class="flex left-8">
       <button aria-label="mk" @click="loadLanguageAsync('mk')" class="rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" width="38" height="32" viewBox="-8 -5 37 35">
@@ -115,6 +90,31 @@ function changeMode() {
         </svg>
       </button>
     </div>
+    <div class="bg-transparent flex place-content-center gap-0 relative self-center left-1">
+      <Link
+          href="/"
+          class="relative top-0 left-3 border-t-2 border-l-2 border-white border-b-2 py-2 px-6 bg-[#C9E6F0] my-auto rounded-2xl rounded-r-none capitalize">
+        {{ $t('menu.home') }}
+      </Link>
+      <div class="relative z-10 border-2 text-white rounded-full">
+        <UpdateButton/>
+      </div>
+      <Link
+          href='/about'
+          class="relative top-0 -left-3 border-t-2 border-r-2 border-white border-b-2 py-2 px-6 bg-[#C9E6F0] my-auto rounded-2xl rounded-l-none capitalize">
+        {{ $t('menu.about') }}
+      </Link>
+    </div>
+    <button @click="darkMode = !darkMode" class="h-12 w-12 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+      <svg class="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+      </svg>
+      <svg class="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
+        <path
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            fill-rule="evenodd" clip-rule="evenodd"></path>
+      </svg>
+    </button>
   </div>
 </template>
 
