2455eb8d46e53c21b98d6bab78007eb07237f425
Darko <d.siracevski@gmail.com>
2024-12-27 16:51:20 +0100

    quickfix

--- resources/js/Components/Custom/Menu.vue
+++ resources/js/Components/Custom/Menu.vue
@@ -106,16 +106,15 @@ function changeMode() {
       </Link>
     </div>
     <div class="flex place-content-end pr-1">
-      <button @click="darkMode = !darkMode" class="h-12 w-12 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-        <svg class="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 -4 22 22">
-          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-        </svg>
-        <svg class="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 -4 22 22">
-          <path
-              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-              fill-rule="evenodd" clip-rule="evenodd"></path>
-        </svg>
-      </button>
+      <label class="relative inline-flex cursor-pointer items-center">
+        <input @click="darkMode = !darkMode" type="checkbox" value="" checked class="peer sr-only" />
+        <div
+            class="peer flex h-8 items-center gap-2 rounded-2xl bg-orange-600 px-2 after:absolute after:left-1 after: after:h-6 after:w-10 after:rounded-full after:bg-white/40 after:transition-all after:content-[''] peer-checked:bg-stone-600 peer-checked:after:translate-x-full peer-focus:outline-none dark:border-slate-600 dark:bg-slate-700 text-sm text-white"
+        >
+          <span>Dark</span>
+          <span>Light</span>
+        </div>
+      </label>
     </div>
   </div>
 </template>
