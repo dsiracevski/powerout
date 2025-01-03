8755aa2d53a0f23ad6e95559b9cfc0553052c1b8
Darko <d.siracevski@gmail.com>
2025-01-03 22:36:04 +0100

    optimizations

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,10 +11,10 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Super Admin',
-            'email' => 'superadmin@powerout.test',
+            'name' => 'Darko Sirachevski',
+            'email' => 'd.siracevski@prekininastruja.mk',
             'email_verified_at' => now(),
-            'password' => Hash::make('password'),
+            'password' => Hash::make('j3b0t3br3'),
         ]);
     }
 }
